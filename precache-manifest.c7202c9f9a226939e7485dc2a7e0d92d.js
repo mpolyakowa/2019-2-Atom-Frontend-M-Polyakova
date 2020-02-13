cd776655bb4460bb1f31ef5c30d009f07e8431ca
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-13 17:23:09 +0000

    Deploy mpolyakowa/2019-2-Atom-Frontend-M-Polyakova to github.com/mpolyakowa/2019-2-Atom-Frontend-M-Polyakova.git:gh-pages

---
+++ precache-manifest.c7202c9f9a226939e7485dc2a7e0d92d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d329fb9f255aa5e541d93544776728",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/index.html"
+  },
+  {
+    "revision": "424235f5cd518bb256ec",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/static/css/main.4f90142b.chunk.css"
+  },
+  {
+    "revision": "b91086194b9457d18c79",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/static/js/2.3764106a.chunk.js"
+  },
+  {
+    "revision": "424235f5cd518bb256ec",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/static/js/main.2bd07656.chunk.js"
+  },
+  {
+    "revision": "03cd374dafda557a35c0",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/static/js/runtime-main.7cb689ba.js"
+  },
+  {
+    "revision": "706cfa3f0821dc3d02c67901143d7d59",
+    "url": "/2019-2-Atom-Frontend-M-Polyakova/static/media/avatar.706cfa3f.svg"
+  }
+]);
